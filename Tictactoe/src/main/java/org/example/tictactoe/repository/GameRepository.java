cdd88bdeb4d2bc408feb482ac87b8bdde56ba86c
Alessandro Pastor <alexkip159alex@gmail.com>
2024-09-25 05:54:29 -0500

    asdsd

---
+++ Tictactoe/src/main/java/org/example/tictactoe/repository/GameRepository.java
@@ -0,0 +1,8 @@
+package org.example.tictactoe.repository;
+
+import org.example.tictactoe.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
+
