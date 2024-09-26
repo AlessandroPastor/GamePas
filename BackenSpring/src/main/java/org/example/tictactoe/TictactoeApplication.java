a7ca7fd3f0628dd8549307018a36109d77a10f97
Alessandro Pastor <alexkip159alex@gmail.com>
2024-09-26 13:20:12 -0500

    asdasd

--- BackenSpring/src/main/java/org/example/tictactoe/TictactoeApplication.java
+++ BackenSpring/src/main/java/org/example/tictactoe/TictactoeApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"org.example.tictactoe.entity"})
 public class TictactoeApplication {
     public static void main(String[] args) {
         SpringApplication.run(TictactoeApplication.class, args);
